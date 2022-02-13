786cef98146de0fd9856ef8d99b98b80d2557e5e
ニワ　ユイマ <89630140+yyyuima0309@users.noreply.github.com>
2022-02-13 13:37:26 +0900

    todoにテキストついかできるようにした

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import "./styles.css";
+
+const onClickAdd = () => {
+  //入力された文字の取得と初期化
+  const inputText = document.querySelector("#add-text").value;
+  document.querySelector("#add-text").value = "";
+
+  //ulの作成
+  const ul = document.createElement("ul");
+  ul.className = "list-row";
+
+  const li = document.createElement("li");
+  li.innerText = inputText;
+
+  //ネスト　ulの子要素にli追加
+  ul.appendChild(li);
+
+  //todoに追加する
+  document.querySelector("#imcomplete-list").appendChild(ul);
+};
+
+document.querySelector("#add-button").addEventListener("click", () => {
+  onClickAdd();
+});
