cb2609ab546b11ef628867d5aec31aca0a8bf7f0
ニワ　ユイマ <89630140+yyyuima0309@users.noreply.github.com>
2022-02-13 14:47:16 +0900

    リストやボタンの追加

--- src/index.js
+++ src/index.js
@@ -9,16 +9,34 @@ const onClickAdd = () => {
   const ul = document.createElement("ul");
   ul.className = "list-row";
 
+  //liの作成
   const li = document.createElement("li");
   li.innerText = inputText;
 
-  //ネスト　ulの子要素にli追加
+  //ボタン生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("aaa");
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    const deleteTarget = deleteButton.parentNode;
+    document.querySelector("#imcomplete-list").removeChild(deleteTarget);
+  });
+
+  //ネスト　ulの子要素としてそれぞれ要素をを追加
   ul.appendChild(li);
+  ul.appendChild(completeButton);
+  ul.appendChild(deleteButton);
 
   //todoに追加する
   document.querySelector("#imcomplete-list").appendChild(ul);
 };
 
+//追加クリックしたらおこるイベント
 document.querySelector("#add-button").addEventListener("click", () => {
   onClickAdd();
 });
